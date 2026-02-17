fb8b93d5ae7d072fc3f88152ff836ac90ae866c3
Puneet Kumar <puneet@akvitechnologies.com>
2026-02-17 15:12:13 +0100

    feat: style update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,23 @@ module.exports = {
 				"background-dark": "#1b1022",
 				background: "var(--background)",
 				foreground: "var(--foreground)",
+				primary: "#14332d",
+				gray: {
+					50: "#f9fafb",
+					100: "#f3f4f6",
+					200: "#e5e7eb",
+					300: "#d1d5db",
+					400: "#9ca3af",
+					500: "#6b7280",
+					600: "#14332d",
+					700: "#14332d",
+					800: "#14332d",
+					900: "#14332d",
+				},
+			},
+			fontFamily: {
+				sans: ["var(--font-poppins)", "ui-sans-serif", "system-ui"],
+				poppins: ["var(--font-poppins)"],
 			},
 			fontFamily: {
 				display: ["Inter", "sans-serif"],
